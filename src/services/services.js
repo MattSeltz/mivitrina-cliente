f53d40f6a5a36d8fa4ca9e0d9282cba65d4b7ca3
Matias Seltzer <107654917+MattSeltz@users.noreply.github.com>
2025-02-03 11:04:40 -0300

    V25

--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,7 @@ import { ENVIRONMENT } from "../configs/configs";
 
 export const getData = async (uri) => {
 	try {
-		const res = await fetch(`${ENVIRONMENT}/${uri}`, {
+		const res = await fetch(`${ENVIRONMENT}${uri}`, {
 			method: "GET",
 			credentials: "include",
 		});
@@ -17,7 +17,7 @@ export const getData = async (uri) => {
 
 export const getOneData = async (uri, id) => {
 	try {
-		const res = await fetch(`${ENVIRONMENT}/${uri}/${id}`, {
+		const res = await fetch(`${ENVIRONMENT}${uri}/${id}`, {
 			method: "GET",
 			credentials: "include",
 		});
@@ -32,7 +32,7 @@ export const getOneData = async (uri, id) => {
 
 export const postData = async (uri, body) => {
 	try {
-		const res = await fetch(`${ENVIRONMENT}/${uri}`, {
+		const res = await fetch(`${ENVIRONMENT}${uri}`, {
 			method: "POST",
 			body: JSON.stringify(body),
 			headers: {
@@ -51,7 +51,7 @@ export const postData = async (uri, body) => {
 
 export const putData = async (uri, id, body) => {
 	try {
-		const res = await fetch(`${ENVIRONMENT}/${uri}/${id}`, {
+		const res = await fetch(`${ENVIRONMENT}${uri}/${id}`, {
 			method: "PUT",
 			body: JSON.stringify(body),
 			headers: {
@@ -70,7 +70,7 @@ export const putData = async (uri, id, body) => {
 
 export const deleteData = async (uri, id) => {
 	try {
-		const res = await fetch(`${ENVIRONMENT}/${uri}/${id}`, {
+		const res = await fetch(`${ENVIRONMENT}${uri}/${id}`, {
 			method: "DELETE",
 			credentials: "include",
 		});
